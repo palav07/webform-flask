d3b0114527e8056e92a211a8b3bb582bda484b52
paul <palav.singh@outlook.com>
2024-11-15 08:17:09 -0800

    final commit for form and connected db.

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "hello12321"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    date=db.Column(db.Date)
+    occupation = db.Column(db.String(50))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        date= request.form['date']
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+        occupation = request.form['occupation']
+
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
+        flash("Your data is submitted successfully!", "success")
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
